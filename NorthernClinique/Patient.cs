755a5c507a19f580e6c6171dc09823ef315ce5d0
Martin Forget <mforget2011@hotmail.com>
2022-10-02 21:26:31 -0400

    sessions, processus, recherche

--- NorthernClinique/Patient.cs
+++ NorthernClinique/Patient.cs
@@ -34,5 +34,6 @@ namespace NorthernClinique
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Admission> Admission { get; set; }
         public virtual Assurance Assurance { get; set; }
+
     }
 }
--- NorthernClinique/RecherchePatient.xaml.cs
+++ NorthernClinique/RecherchePatient.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,28 +15,18 @@ using System.Windows.Shapes;
 namespace NorthernClinique
 {
     /// <summary>
-    /// Logique d'interaction pour sessionMedecin.xaml
+    /// Logique d'interaction pour RecherchePatient.xaml
     /// </summary>
-    public partial class sessionMedecin : Window
+    public partial class RecherchePatient : Window
     {
-        Northern_Lights_HospitalEntities1 BDD;
-        public sessionMedecin()
+        public RecherchePatient()
         {
             InitializeComponent();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            BDD = new Northern_Lights_HospitalEntities1();
 
-            var query =
-                from a in BDD.Admission
-                join p in BDD.Patient on a.NSS equals p.NSS
-                select new { a.NSS, p.nom, p.prenom, a.IDMedecin, a.Numero_lit, a.date_admission, a.date_du_congé };
-
-            dgConge.DataContext = query.ToList(); 
         }
-
-        
     }
 }
