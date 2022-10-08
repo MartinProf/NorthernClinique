b66760044d12d88e1ddd53b591c5effa755e8b2f
WIN10 <WIN10@DESKTOP-QUFDOFK>
2022-10-08 11:51:12 -0400

    Admission OK

--- NorthernClinique/ProcessusAdmission.xaml.cs
+++ NorthernClinique/ProcessusAdmission.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -19,10 +21,113 @@ namespace NorthernClinique
     /// </summary>
     public partial class ProcessusAdmission : Window
     {
+        Northern_Lights_HospitalEntities1 myBDD;
         public ProcessusAdmission()
         {
             InitializeComponent();
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            myBDD = new Northern_Lights_HospitalEntities1();
+
+            Patient patient = cbNSS.SelectedItem as Patient;
+            cbNSS.DataContext = myBDD.Patient.ToList();
+
+            Medecin medecin = cboMedecin.SelectedItem as Medecin;
+            cboMedecin.DataContext = myBDD.Medecin.ToList();
+
+            Departement departement = cboTypeChambre.SelectedItem as Departement;
+            cboTypeChambre.DataContext = myBDD.Departement.ToList();
+
+            var queryTypeLit =
+                (from a in myBDD.Departement
+                 join b in myBDD.Lit on a.IDDepartement equals b.IDDepartement
+                 join c in myBDD.Type_Lit on b.IDType equals c.IDType
+                 where (b.occupe == false)
+                 select new { b.Numero_lit, a.nom_departement, c.description
+                }).ToList();
+            
+            cboNumLit.DataContext = queryTypeLit;
+            cboTypeLit.DataContext = queryTypeLit;
+
+        }
+
+        private void btnValiderAdmission_Click(object sender, RoutedEventArgs e)
+        {
+            Admission admission = new Admission();
+
+            if (checkbChirurgie.IsChecked == true) {
+                try
+                {
+                    admission.chirurgie_programmee = true;
+                    admission.date_chirurgie = datepChirurgie.SelectedDate;
+                }
+                catch (Exception)
+                {
+
+                    MessageBox.Show("Vous devez entrer une date de chirurgie!");
+                }
+            }
+            
+            admission.date_admission = DateTime.Now;
+            if(cBoxTelevision.IsChecked == true) admission.televiseur = true;
+            if (cbTelephone.IsChecked == true) admission.telephone = true;
+            admission.NSS = int.Parse(cbNSS.Text);
+            admission.Numero_lit = int.Parse(cboNumLit.Text);
+            admission.IDMedecin = int.Parse(cboMedecin.Text);
+
+            myBDD.Admission.Add(admission);
+            try
+            {
+                myBDD.SaveChanges();
+                MessageBox.Show("L'admission a été fait avec succès");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+            cBoxTelevision.IsChecked = false;
+            cbTelephone.IsChecked = false;
+            checkbChirurgie.IsChecked = false;
+        }
+
+        private void cboTypeChambre_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int indice = cboTypeChambre.SelectedIndex+1;
+
+            var queryTypeLit =
+                (from a in myBDD.Departement
+                 join b in myBDD.Lit on a.IDDepartement equals b.IDDepartement
+                 join c in myBDD.Type_Lit on b.IDType equals c.IDType
+                 where (b.occupe == false)
+                 where (a.IDDepartement == indice)
+                 select new
+                 {
+                     b.Numero_lit,
+                     a.nom_departement,
+                     c.description
+                 }).Distinct().ToList();
+
+            cboNumLit.DataContext = queryTypeLit;
+            cboTypeLit.DataContext = queryTypeLit;
+
+        }
+
+        private void cbNSS_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Patient patient = cbNSS.SelectedItem as Patient;
+            txtNom.Text = patient.nom.ToString();
+            txtPrenom.Text = patient.prenom.ToString();
+        }
+
+        private void cboMedecin_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Medecin medecin = cboMedecin.SelectedItem as Medecin;
+            txtBoxMedNom.Text = medecin.nom.ToString();
+            txtBoxMedPrenom.Text = medecin.prenom.ToString();
+        }
     }
 }
--- NorthernClinique/RecherchePatient.xaml.cs
+++ NorthernClinique/RecherchePatient.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,7 @@ namespace NorthernClinique
     /// </summary>
     public partial class RecherchePatient : Window
     {
+        Northern_Lights_HospitalEntities1 myBDD;
         public RecherchePatient()
         {
             InitializeComponent();
@@ -26,16 +29,61 @@ namespace NorthernClinique
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            myBDD = new Northern_Lights_HospitalEntities1();
+            cboAssurance.DataContext = myBDD.Assurance.ToList();
+        }
+
+        private void btnAdmission_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessusAdmission processusAdmission = new ProcessusAdmission();
+            processusAdmission.Show();
+            this.Close();
+
+        }
+
+        private void cboAssurance_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            Assurance assurance = cboAssurance.SelectedItem as Assurance;
+
+            cboAssurance.DataContext = myBDD.Assurance.ToList();
 
         }
 
         private void btnAjouterPatient_Click(object sender, RoutedEventArgs e)
         {
+            Patient patient = new Patient();
+            patient.date_naissance = DPdateNaissance.SelectedDate.Value;
+            patient.nom = txtNom.Text;
+            patient.prenom = txtPrenom.Text;
+            patient.adresse = txtAdresse.Text;
+            patient.ville = txtVille.Text;
+            patient.province = txtProvince.Text;
+            patient.code_postal = txtCodePostal.Text;
+            patient.telephone = txtTelephone.Text;
+            patient.IDAssurance = ((Assurance)cboAssurance.SelectedItem).IDAssurance;
 
-        }
+            myBDD.Patient.Add(patient);
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+            try
+            {
+                myBDD.SaveChanges();
+                MessageBox.Show("Nouveau patient ajouter avec succès");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+            DPdateNaissance.DataContext = DateTime.Today;
+            txtNom.Text = string.Empty;
+            txtPrenom.Text = string.Empty;
+            txtAdresse.Text = string.Empty;
+            txtVille.Text = string.Empty;
+            txtProvince.Text = string.Empty;
+            txtCodePostal.Text = string.Empty;
+            txtTelephone.Text = string.Empty;
 
         }
     }
